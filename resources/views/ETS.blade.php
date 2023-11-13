cdface261df0ded6f1e421edcbc1f35f9a4a64db
baringga <144636217+baringga@users.noreply.github.com>
2023-11-13 15:30:40 +0700

    add
    add

---
+++ resources/views/ETS.blade.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Baringga Aurico De Erwada_502622113_PWEB_D</title>
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="vendor/aos/aos.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <script>
+    function Send() {
+        var email = document.getElementById('email');
+        if (email.value == ""){
+                alert('Email must be filled in!');
+                return false;
+        }else if (namee.value == ""){
+                alert('Name must be filled in!');
+                return false;
+        }else if (message.value == ""){
+                alert('Message must be filled in!');
+                return false;
+        }else{
+            alert('Message successfully sent.');
+        }
+      }
+  </script>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1><a href="#hero">Pemrograman Web D</a></h1>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active" href="#heroPage">Home</a></li>
+          <li><a href="#AboutPage">About</a></li>
+          <li><a href="#SelfDevelopmentPage">Self-Development</a></li>
+          <li><a href="#PortofolioPage">Portofolio</a></li>
+          <li><a href="#ContactPage">Contact Me</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+
+<section class="hero-section" id="heroPage">
+  <div class="wave">
+    <svg width="100%" height="355px" viewBox="0 0 1920 355" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Apple-TV" transform="translate(0.000000, -402.000000)" fill="#FFFFFF">
+          <path d="M0,439.134243 C175.04074,464.89273 327.944386,477.771974 458.710937,477.771974 C654.860765,477.771974 870.645295,442.632362 1205.9828,410.192501 C1429.54114,388.565926 1667.54687,411.092417 1920,477.771974 L1920,757 L1017.15166,757 L0,757 L0,439.134243 Z" id="Path"></path>
+        </g>
+      </g>
+    </svg>
+  </div>
+
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-12 hero-text-image">
+        <div class="row">
+          <div class="col-lg-8 text-center text-lg-start">
+            <h1 data-aos="fade-right">Baringga Aurico De Erwada</h1>
+            <p class="mb-5" data-aos="fade-right" data-aos-delay="100">Hi! I am currently studying at the Sepuluh Nopember Institute of Technology</p>
+            <p data-aos="fade-right" data-aos-delay="200" data-aos-offset="-500"><a href="#AboutPage" class="btn btn-outline-white">Know Me!</a></p>
+          </div>
+          <div class="col-lg-4 iphone-wrap">
+            <img src="img/foto.png" alt="Image" class="phone-2" data-aos="fade-right" data-aos-delay="200"> <br><br><br><br><br><br><br><br><br>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section><!-- End Hero -->
+
+<br>
+<br>
+<br>
+<br>
+
+<section class="section pb-0" id="AboutPage">
+  <div class="container">
+  <div class="row justify-content-center text-center mb-5" >
+    <div class="col-md-5" data-aos="fade-up">
+      <h2 class="section-heading" >About Me</h2>
+    </div>
+    </div>
+      <div class="row align-items-center">
+        <div class="col-md-4 me-auto">
+          <h2 class="mb-4">About Me</h2>
+          <p class="mb-4 text-justify">Baringga Aurico De Erwada (19 years old) is an undergraduate student of the Information Systems Department of Institut Teknologi Sepuluh Nopember 2022. An excellent relationship builder with a track record of Educational History, organizations, committees, and internship experiences. I always ready for learning new things and facing new challenges. I also determined to upgrade my knowledge and my soft skills during my study in ITS. With a supportive family background, I’ve grown into a strong, optimistic, empathetic and adaptable person in a new environment.</p>
+          <p><a href="#SelfDevelopmentPage">Read More</a></p>
+        </div>
+        <div class="col-md-6" data-aos="fade-left">
+          <img src="img/foto2.jpg" alt="Image" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section class="section" id="SelfDevelopmentPage">
+      <div class="container">
+
+        <div class="row justify-content-center text-center mb-5">
+          <div class="col-md-5" data-aos="fade-up">
+            <h2 class="section-heading">Self-Development</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-building-fill"></i>
+              </div>
+              <h3 class="mb-3">Campus</h3>
+              <p>I'm an undergraduate student of the information System Departmen batch 2022</p>
+            </div>
+          </div>
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-clipboard-check"></i>
+              </div>
+              <h3 class="mb-3">Experiences</h3>
+              <p>Intern Staff of Business Development KAMI Foundation</p>
+            </div>
+          </div>
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-folder"></i>
+              </div>
+              <h3 class="mb-3">Organizations</h3>
+              <p>Staff of Sosmas BEM ITS</p>
+            </div>
+          </div>
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-battery "></i>
+              </div>
+              <h3 class="mb-3">Committees</h3>
+              <p>Information System Expo ITS</p>
+            </div>
+          </div>
+
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-chat-left-text-fill"></i>
+              </div>
+              <h3 class="mb-3">Skills</h3>
+              <p>I'm adaptive, communicative, and have a leadership </p>
+            </div>
+          </div>
+          <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="feature-1 text-center">
+              <div class="wrap-icon icon-1">
+                <i class="bi bi-bar-chart-fill"></i>
+              </div>
+              <h3 class="mb-3">Achievements</h3>
+              <p>Awardee of Program Beasiswa Santri Berprestasi from Ministry of Religious Affair </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="section" id="PortofolioPage">
+
+      <div class="container">
+        <div class="row justify-content-center text-center mb-5" id="portofolio">
+          <div class="col-md-5" data-aos="fade-up">
+            <h2 class="section-heading">My Portofolio</h2>
+          </div>
+        </div>
+
+        <div class="row  justify-content-center text-center mb-5">
+          <div class="col-md-3">
+            <div class="step">
+              <span class="number">01</span>
+              <h3>Profile Poster</h3>
+              <img src="img/Portofolio1.png" alt="Image" class="img-fluid" data-aos="fade-right" data-aos-delay="200">
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="step">
+              <span class="number">02</span>
+              <h3>Infographic</h3>
+              <img src="img/Portofolio2.png" alt="Image" class="img-fluid" data-aos="fade-right" data-aos-delay="200">
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="step">
+              <span class="number">03</span>
+              <h3>Creative CV</h3>
+              <img src="img/Portofolio3.png" alt="Image" class="img-fluid" data-aos="fade-right" data-aos-delay="200">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section" id="ContactPage">
+      <div class="container">
+        <div class="row mb-5 align-items-end">
+          <div class="col-md-6" data-aos="fade-up">
+            <h2><b>Contact Form</b></h2>
+            <p class="mb-0">Give your suggestions and criticism</p>
+          </div>
+
+          </div>
+    <!-- contactPage-->
+            <div class="row">
+              <div class="col-md-4 ms-auto order-2" data-aos="fade-up">
+                <ul class="list-unstyled">
+                  <li class="mb-3">
+                    <strong class="d-block mb-1">Address</strong>
+                    <span>Jl. Arief Rahman Hakim No.9, Keputih, Kec. Sukolilo, Surabaya, Jawa Timur 60111</span>
+                  </li>
+                  <li class="mb-3">
+                    <strong class="d-block mb-1">Phone</strong>
+                    <span>+6285850787836</span>
+                  </li>
+                  <li class="mb-3">
+                    <strong class="d-block mb-1">Email</strong>
+                    <span>baringgaau@gmail.com</span>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-md-6 mb-5 mb-md-0" data-aos="fade-up">
+                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+
+                  <div class="row">
+                    <div class="col-md-6 form-group">
+                      <label for="name">Name</label>
+                      <input type="text" id="namee" name="name" class="form-control" id="name" required>
+                    </div>
+                    <div class="col-md-6 form-group mt-3 mt-md-0">
+                      <label for="name">Email</label>
+                      <input type="email" id="email" class="form-control" name="email" id="email" required>
+                    </div>
+                    <div class="col-md-12 form-group mt-3">
+                      <label for="name">Subject</label>
+                      <input type="text" class="form-control" name="subject" id="subject" required>
+                    </div>
+                    <div class="col-md-12 form-group mt-3">
+                      <label for="name">Message</label>
+                      <textarea id="message" class="form-control" name="message" required></textarea>
+                    </div>
+
+                    <div class="col-md-12 mb-3">
+                      <div class="loading">Loading</div>
+                      <div class="error-message"></div>
+                      <div class="sent-message">Your message has been sent. Thank you!</div>
+                    </div>
+
+                    <div class="col-md-6 form-group">
+                      <input type="button" class="btn btn-primary d-block w-100" value="Send Message" onclick="Send();">
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+
+  <!-- ======= Footer ======= -->
+  <footer class="footer" role="contentinfo">
+    <div class="container">
+      <div class="row justify-content-center text-center mb-5">
+        <div class="col-md-4 mb-4 mb-md-0">
+          <p><h3><b>My Social Media</b></h3>
+          <p>Come connect with me!</p>
+          <p class="social">
+            <a href="https://www.instagram.com/baringgaau/"><span class="bi bi-instagram"></span></a>
+            <a href="https://www.linkedin.com/in/baringgaau/"><span class="bi bi-linkedin"></span></a>
+            <a href="#"><span class="bi bi-twitter"></span></a>
+            <a href="#"><span class="bi bi-facebook"></span></a>
+            <a href="#"><span class="bi bi-tiktok"></span></a>
+            <a href="#"><span class="bi bi-youtube"></span></a>
+          </p>
+          <div class="row justify-content-center text-center">
+            <div class="col-md-7">
+              <p class="copyright">&copy;Baringga Aurico De Erwada</p>
+              <div class="credits">
+                Designed by <a href="https://www.instagram.com/baringgaau/">@baringgaau</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Vendor JS Files -->
+  <script src="vendor/aos/aos.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
